e9e21316d2f381b846623e7dc451c9665d6d7ab5
vivien <33899359+Wiblox@users.noreply.github.com>
2022-01-06 10:21:00 +0100

    Four Commit
    Page de detail fonctionnel.

--- src/api/TheMovieDBAPI.jsx
+++ src/api/TheMovieDBAPI.jsx
@@ -3,12 +3,25 @@ const API_KEY = "f60dc1588d1b92e483f83fa137b9f5ab";
 export async function getPopularPeople() {
   try {
     const myHeaders = new Headers({ "user-key": API_KEY });
-    const url = `https://api.themoviedb.org/3/person/popular?api_key=${API_KEY}&language=en-US&page=1`;
+    const url = `https://api.themoviedb.org/3/person/popular?api_key=${API_KEY}&language=fr-FR&page=1`;
     const response = await fetch(url, { headers: myHeaders });
     const json = await response.json();
     return json;
   } catch (error) {
     console.log(`Error with function getPopularPeople ${error.message}`);
+    throw error;
+  }
+}
+
+export async function getPopularPeopleDetails(peopletID) {
+  try {
+    const myHeaders = new Headers({ "user-key": API_KEY });
+    const url = `https://api.themoviedb.org/3/person/${peopletID}?api_key=${API_KEY}&language=fr-FR`;
+    const response = await fetch(url, { headers: myHeaders });
+    const json = await response.json();
+    return json;
+  } catch (error) {
+    console.log(`Error with function getRestaurantDetails ${error.message}`);
     throw error;
   }
 }
--- src/components/People.jsx
+++ src/components/People.jsx
@@ -1,8 +1,63 @@
 import React from "react";
-import { View, TextInput, Button } from "react-native";
+import { View, ActivityIndicator, TextInput, Text, Button } from "react-native";
+import { useState, useEffect } from "react";
+import DisplayError from "../components/DisplayError";
+import PersonnetListItem from "../components/PersonnelistItem";
+import { getPopularPeopleDetails } from "../api/TheMovieDBAPI";
+import { StyleSheet, FlatList, Keyboard } from "react-native";
 
-const People = ({ route, favRestaurants, dispatch }) => {
-  return <View></View>;
+const People = ({ route, dispatch }) => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [People, setPeople] = useState(null);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    requestRestaurant();
+  }, []); // Uniquement à l'initialisation
+
+  // Pourrait être directement déclarée dans useEffect
+  const requestRestaurant = async () => {
+    try {
+      const zomatoRestaurantResult = await getPopularPeopleDetails(
+        route.params.PeopleID
+      );
+      console.log(route.params.PeopleID);
+
+      setPeople(zomatoRestaurantResult);
+      setIsLoading(false);
+      console.log(zomatoRestaurantResult);
+    } catch (error) {
+      console.log(error);
+      setIsError(true);
+    }
+  };
+
+  return (
+    <View>
+      {isError ? (
+        <DisplayError message="Impossible de récupérer les données du restaurants" />
+      ) : isLoading ? (
+        <View>
+          <ActivityIndicator size="large" />
+        </View>
+      ) : (
+        <View>
+          <View>
+            <Text>{People.name}</Text>
+          </View>
+          <View>
+            <Text>{People.birthday}</Text>
+          </View>
+          <View>
+            <Text>{People.also_known_as}</Text>
+          </View>
+          <View>
+            <Text>{People.biography}</Text>
+          </View>
+        </View>
+      )}
+    </View>
+  );
 };
 
 export default People;
